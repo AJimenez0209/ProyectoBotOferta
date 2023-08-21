a8ff2017dab88c308812e166089abcca73e6ca98
AaronJ <jimenezaaron5@gmail.com>
2023-08-21 14:47:16 -0400

    Codigo Funcional con mejoras en la busqueda y con muestra de categorías

--- BotBD1.4.py
+++ BotBD1.4.py
@@ -8,14 +8,21 @@ from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, CommandHandler, Updater
 import telegram
 import logging
+import asyncio
+import aiohttp
+import aiosqlite
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 print(telegram.__version__)
 
+
 token = '6302321641:AAHgQuqA62sGofOLwrHFvmP2FPm29B2jf6c'  # Mantén este valor secreto
 
+async def fetch(url, session):
+    async with session.get(url) as response:
+        return await response.text()
 
 def create_table():
     with sqlite3.connect('productos.db') as conn:
@@ -29,6 +36,7 @@ def create_table():
             category TEXT
         )
         ''')
+        conn.commit()  # Confirmación explícita
 
 
 def column_exists(table_name, column_name):
@@ -51,13 +59,13 @@ def get_unique_categories():
         cursor = conn.cursor()
         cursor.execute("SELECT DISTINCT category FROM productos")
         categories = [row[0] for row in cursor.fetchall()]
-        print("Categorias extraidas:", categories)
+        #print("Categorias extraidas:", categories) depuracion
         return categories
 
 
 def build_category_keyboard(categories):
     keyboard = [[InlineKeyboardButton(category, callback_data=category)] for category in categories]
-    print("Teclado construido:", keyboard)  # <-- Añade esta línea para depuración
+    #print("Teclado construido:", keyboard)  # <-- Añade esta línea para depuración
     return InlineKeyboardMarkup(keyboard)
 
 def get_products_by_category(category):
@@ -77,7 +85,7 @@ def add_discount_percentage_column():
             except sqlite3.Error as e:
                 print(f"Error al añadir la columna discount_percentage: {e}")
 
-def get_elements(soup):
+async def get_elements(soup):
     # Buscar clases que comienzan con "jsx"
     jsx_elements = soup.find_all(class_=lambda x: x and x.startswith("jsx"))
     
@@ -87,6 +95,8 @@ def get_elements(soup):
 
     return jsx_elements + pod_link_elements + grid_view_elements
 
+    
+   
 
 def clean_price(price_str):
     # Limpiamos la cadena eliminando caracteres no deseados, excepto el punto y dígitos
@@ -107,93 +117,100 @@ def find_element_by_classes(element, tag, classes_to_search):
     return None
 
 
-def get_offers():
+async def get_offers():
     url_base = "https://www.falabella.com/falabella-cl/collection/ofertas"
     new_products = []  # Lista para guardar los nuevos productos encontrados
 
-    for page in range(1, 2):
-        url = f"{url_base}?page={page}"
-        try:
-            response = requests.get(url)
-            response.raise_for_status()  # Esto generará un error si el código de estado no es 200
-
-            soup = BeautifulSoup(response.text, "html.parser")
-            all_elements = get_elements(soup)
-
-            with sqlite3.connect('productos.db') as conn:
-                cursor = conn.cursor()
-
-                for element in all_elements:
-                    product_name = element.find("b", class_="jsx-1833870204 copy2 primary jsx-2889528833 normal pod-subTitle subTitle-rebrand")
-                    # Definimos las clases que deseamos buscar
-                    classes_to_search = [
-                    "copy10 primary high jsx-2889528833 normal line-height-22",
-                    "copy10 primary medium jsx-2889528833 normal line-height-22"
-                    ]
-
-                    # Buscamos el elemento que tenga alguna de las clases definidas
-                    offer = find_element_by_classes(element, None, classes_to_search)
-
-                    discount_badge = element.find("div", class_="jsx-2575670149 discount-badge")
+    async with aiohttp.ClientSession() as session:
+        for page in range(1, 2):
+            url = f"{url_base}?page={page}"
             
-                    # Definimos las clases que deseamos buscar para el precio anterior
-                    classes_to_search_price = [
-                    "copy3 septenary medium jsx-2889528833 normal crossed line-height-17",
-                    "copy3 primary medium jsx-2889528833 normal crossed line-height-17"
-                    ]
+            
+            try:
+                async with session.get(url) as response:
+                    response.raise_for_status()  # Esto generará un error si el código de estado no es 200
+                    html = await fetch(url, session)
+                    soup = BeautifulSoup(html, "html.parser")
+                    all_elements = await get_elements(soup)
 
-                    previous_price_element = find_element_by_classes(element, "span", classes_to_search_price)
-                    previous_price = clean_price(previous_price_element.text.strip()) if previous_price_element else None
+                async with aiosqlite.connect('productos.db') as conn:
+                    cursor = await conn.cursor()
+                     
 
-                    # Extraer el porcentaje de descuento
-                    discount_percentage = discount_badge.text.strip() if discount_badge else "N/A"
+                    for element in all_elements:
+                        product_name = element.find("b", class_="jsx-1833870204 copy2 primary jsx-2889528833 normal pod-subTitle subTitle-rebrand")
+                        # Definimos las clases que deseamos buscar
+                        classes_to_search = [
+                        "copy10 primary high jsx-2889528833 normal line-height-22",
+                        "copy10 primary medium jsx-2889528833 normal line-height-22"
+                        ]
 
-                    if product_name and offer and discount_badge:
-                        div_element = element.find("div", class_="jsx-1833870204 jsx-3831830274 pod pod-4_GRID")
-                        data_category = div_element["data-category"] if div_element and "data-category" in div_element.attrs else None
+                        # Buscamos el elemento que tenga alguna de las clases definidas
+                        offer = find_element_by_classes(element, None, classes_to_search)
 
-                        link_element = element.find("a", class_="jsx-2907167179 layout_grid-view layout_view_4_GRID")
-                        link = link_element["href"] if link_element else None
+                        discount_badge = element.find("div", class_="jsx-2575670149 discount-badge")
+            
+                        # Definimos las clases que deseamos buscar para el precio anterior
+                        classes_to_search_price = [
+                        "copy3 septenary medium jsx-2889528833 normal crossed line-height-17",
+                        "copy3 primary medium jsx-2889528833 normal crossed line-height-17"
+                        ]
 
-                        product_name_text = product_name.text.strip().split("$")[0].strip()
-                        current_offer = clean_price(offer.text.strip())
+                        previous_price_element = find_element_by_classes(element, "span", classes_to_search_price)
+                        previous_price = clean_price(previous_price_element.text.strip()) if previous_price_element else None
 
-                        cursor.execute("SELECT * FROM productos WHERE nombre = ?", (product_name_text,))
-                        product_in_db = cursor.fetchone()
+                        # Extraer el porcentaje de descuento
+                        discount_percentage = discount_badge.text.strip() if discount_badge else "N/A"
 
-                        logging.info(f"Producto detectado: {product_name_text}, Oferta: {current_offer}, Precio anterior: {previous_price}, Descuento: {discount_percentage}, Categoría: {data_category}, Enlace: {link}")
-                        last_price = clean_price(product_in_db[3]) if product_in_db and product_in_db[3] else None
+                        product_in_db = None
+                        product_name_text = None
+                        current_offer = None
 
+                        if product_name and offer and discount_badge:
+                           div_element = element.find("div", class_="jsx-1833870204 jsx-3831830274 pod pod-4_GRID")
+                           data_category = div_element["data-category"] if div_element and "data-category" in div_element.attrs else None
 
-    # TODO: Aquí puedes agregar el código para guardar o actualizar el producto en la base de datos
+                           link_element = element.find("a", class_="jsx-2907167179 layout_grid-view layout_view_4_GRID")
+                           link = link_element["href"] if link_element else None
 
-                        sleep_time = random.uniform(3, 7)  # Pausa durante un tiempo aleatorio entre 3 y 7 segundos
-                        time.sleep(sleep_time)
+                           product_name_text = product_name.text.strip().split("$")[0].strip()
+                           current_offer = clean_price(offer.text.strip())
 
-                        if not product_in_db:
-                            try:
-                                cursor.execute("INSERT INTO productos VALUES (?, ?, ?, ?, ?, ?)", (product_name_text, current_offer, previous_price, discount_percentage, data_category, link))
-                                logging.info(f"Producto {product_name_text} agregado a la base de datos.")
-                                new_products.append((product_name_text, current_offer, previous_price, discount_percentage, data_category, link))
-                            except sqlite3.Error as e:
-                                logging.error(f"Error al agregar el producto {product_name_text} a la base de datos: {e}")
-        # Nota: No es necesario el conn.close() aquí porque el 'with' lo maneja automáticamente
+                           await cursor.execute("SELECT * FROM productos WHERE nombre = ?", (product_name_text,))
+                           product_in_db = await cursor.fetchone()
 
+                           logging.info(f"Producto detectado: {product_name_text}, Oferta: {current_offer}, Precio anterior: {previous_price}, Descuento: {discount_percentage}, Categoría: {data_category}, Enlace: {link}")
+                           last_price = clean_price(product_in_db[3]) if product_in_db and product_in_db[3] else None
 
-        except requests.HTTPError:
-            logging.error(f"Error {response.status_code}. Pausando durante 60 segundos antes de reintentar.")
-            time.sleep(60)
-            continue   # Esto hará que el bucle vuelva al inicio y reintente la misma página
+                           sleep_time = random.uniform(3, 7)  # Pausa durante un tiempo aleatorio entre 3 y 7 segundos
+                           await asyncio.sleep(sleep_time)
+
+                        if not product_in_db and product_name_text and product_name_text != "None":
+                                try:
+                                    await cursor.execute("INSERT INTO productos VALUES (?, ?, ?, ?, ?, ?)", (product_name_text, current_offer, previous_price, discount_percentage, data_category, link))
+                                    await conn.commit()
+                                    logging.info(f"Producto {product_name_text} agregado a la base de datos.")
+                                    new_products.append((product_name_text, current_offer, previous_price, discount_percentage, data_category, link))
+                                except sqlite3.Error as e:
+                                    logging.error(f"Error al agregar el producto {product_name_text} a la base de datos: {e}")
+                                except Exception as e:  # Captura cualquier otra excepción
+                                    logging.error(f"Error inesperado al agregar el producto {product_name_text} a la base de datos: {e}")
+                        else:
+                            logging.error(f"Producto inválido detectado o ya existe en la base de datos: {product_name_text}")
+
+            except aiohttp.ClientResponseError as e:
+                logging.error(f"Error {e.status}. Pausando durante 60 segundos antes de reintentar.")
+                await asyncio.sleep(60)
+                continue  # Esto hará que el bucle vuelva al inicio y reintente la misma página
 
     return new_products  # Devolvemos solo los nuevos productos
-
-
+                       
 def obtener_ofertas():
     """Esta función se encargará de obtener las ofertas."""
     product_offers_links = get_offers()
     return product_offers_links
 
-def enviar_mensajes_telegram(update, context, product_offers_links):
+async def enviar_mensajes_telegram(update, context, product_offers_links):
     """Esta función se encargará de enviar los mensajes a través de Telegram."""
     chat_id = update.effective_chat.id
 
@@ -213,28 +230,42 @@ def enviar_mensajes_telegram(update, context, product_offers_links):
                     logging.error(f"Error al enviar el mensaje a través de Telegram: {e}")
 
             # Espera 2 minutos antes de enviar el siguiente grupo
-            time.sleep(120)
+            await asyncio.sleep(120)
         
         # Mostramos un mensaje al usuario indicando que la búsqueda ha finalizado
         context.bot.send_message(chat_id, "Búsqueda de ofertas completada.")
     else:
         context.bot.send_message(chat_id, "Lo siento, no se pudieron obtener las ofertas en este momento.")
 
-def buscar_ofertas(update, context):
+
+async def asincrono_buscar_ofertas(update, context):
     chat_id = update.effective_chat.id
     context.bot.send_message(chat_id, "Buscando ofertas, por favor espera...")
-    
-    # Primero obtenemos las ofertas
-    product_offers_links = get_offers()  # Guardamos los resultados en una variable
-    
-    # Luego enviamos los mensajes de las ofertas encontradas
-    enviar_mensajes_telegram(update, context, product_offers_links)  # Pasamos los resultados a la función
+
+    # Espera a que se complete get_offers antes de continuar
+    product_offers_links = await get_offers()
+
+    # Ahora puedes usar product_offers_links como una lista regular,
+    # porque get_offers ha completado su ejecución y ha devuelto su resultado.
+    await enviar_mensajes_telegram(update, context, product_offers_links)
     
     # Ahora, mostramos las categorías al usuario
     categories = get_unique_categories()
-    print("Enviando categorias:", categories)  # <-- Añade esta línea para depuración
     keyboard = build_category_keyboard(categories)
     context.bot.send_message(chat_id, "Por favor, selecciona una categoría:", reply_markup=keyboard)
+
+    # Después de esto, el bot no inicia automáticamente una nueva búsqueda,
+    # sino que espera a que el usuario seleccione una categoría o envíe un nuevo comando.
+
+def buscar_ofertas(update, context):
+    # Crea un nuevo bucle de eventos
+    loop = asyncio.new_event_loop()
+    
+    # Establece este bucle como el bucle de eventos actual
+    asyncio.set_event_loop(loop)
+    
+    # Usa este bucle para ejecutar tu corutina
+    loop.run_until_complete(asincrono_buscar_ofertas(update, context))
 
 
 def category_callback(update, context):
@@ -258,7 +289,7 @@ def main():
     updater = Updater(token, use_context=True)
 
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('categorias', buscar_ofertas))
+    
     dp.add_handler(CallbackQueryHandler(category_callback))
     dp.add_handler(CommandHandler('buscar_ofertas', buscar_ofertas))
 
@@ -281,4 +312,6 @@ if __name__ == "__main__":
     # Llama a estas funciones al inicio de tu script para asegurarte de que la tabla exista
     create_table()
     add_discount_percentage_column()
+    #loop = asyncio.get_event_loop()
+    #new_products = loop.run_until_complete(get_offers())
     main()
